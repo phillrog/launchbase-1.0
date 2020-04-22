34669308594256a12286f0acbb78e4cee8df4cbb
fh.proger <phillipe.souza@rte.com.br>
2020-04-22 20:09:54 -0300

    Template engine nunjuck

--- modulo-3/server.js
+++ modulo-3/server.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
 
 const server = express();
+
+server.use(express.static('public'));
+
+server.set("view engine", "html");
+
+nunjucks.configure("views", {
+    express: server
+})
 
 const port = 5000 || server.port;
 
 server.get('/', function(req,res){
-    return res.send('Olá mundo');
+    return res.render("about");
+});
+
+server.get('/cursos', function(req,res){
+    return res.render("courses");
 });
 
 server.listen(port, () => {
