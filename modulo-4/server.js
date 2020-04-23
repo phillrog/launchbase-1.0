b6be9f4b0e5a8c6a43b9a0fb8475e93161ecd6e7
fh.proger <phillipe.souza@rte.com.br>
2020-04-23 20:36:47 -0300

    Iniciando o projeto

--- modulo-4/server.js
+++ modulo-4/server.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
-
+const routes = require('./routes');
 const server = express();
-const videos = require('./data');
-
 
 server.use(express.static('public'));
-
+server.use(routes);
 server.set("view engine", "njk");
 
 nunjucks.configure("views", {
@@ -17,38 +15,7 @@ nunjucks.configure("views", {
 
 const port = 5000 || server.port;
 
-server.get('/', function(req,res){
-    const about =  {
-        avatar_url: "https://image.freepik.com/free-icon/human-skull-with-crossed-bones-silhouette_318-46669.png",
-        name: "Phillipe",
-        role: " Programador full-stack",
-        description: 'Atualmente estudando novas tecnologias <a href="https://github.com/phillrog/launchbase-1.0"  target="_blank">Repositório</a>',
-        links: [{
-            name: 'Github',
-            url: 'https://github.com/phillrog'
-        },
-        {
-            name: 'LinkedIn',
-            url: 'https://linkedin.com'
-        }]
-    }
-    return res.render("about", { about });
-});
 
-server.get('/cursos', function(req,res){
-    return res.render("courses", {items: videos});
-});
-
-server.get('/video', function(req,res){
-    const id = req.query.id;
-    const video = videos.find((v) => v.id == id);
-
-    if (!video) {
-        return res.send('Video not found');
-    }
-
-    return res.render('video', { item: video });
-});
 
 server.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`)
