b1b995e2a503b63485bfee9d45259e1139e2605d
fh.proger <phillipe.souza@rte.com.br>
2020-04-28 13:40:13 -0300

    Salvando dados

--- modulo-4/instructors.js
+++ modulo-4/instructors.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const data = require('./data');
+
 // post
 exports.post = function(req,res) {
     const keys = Object.keys(req.body);
@@ -5,5 +8,16 @@ exports.post = function(req,res) {
         if (req.body[i] == "")
             return res.send(`Please fill all fields!`)
     });
+
+    req.body.birth = Date.parse(req.body.birth);
+    req.body.created_at = Date.now();
+    data.instructors.push(req.body);
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if(err) return res.send("Write file has error");
+
+        return res.redirect('/instructors');
+    });
+
     return res.send(req.body);
 }
