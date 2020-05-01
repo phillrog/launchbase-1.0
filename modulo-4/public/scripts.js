0098fb389f4358a15bf8b8ecae23bb63bbf7569e
fh.proger <phillipe.souza@rte.com.br>
2020-05-01 16:52:06 -0300

    Lógica do Menu Ativo

--- modulo-4/public/scripts.js
+++ modulo-4/public/scripts.js
@@ -1,3 +1,7 @@
 const currentPage = location.pathname;
+const menuItems = document.querySelectorAll('header .links a');
 
-console.log(currentPage);
+for ( item of menuItems ){
+    if (currentPage.includes(item.getAttribute("href")))
+        item.classList.add('active');
+}
