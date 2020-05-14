ea82eef281131e226c49e64566b3a248354f0cd0
fh.proger <phillipe.souza@rte.com.br>
2020-05-13 21:13:43 -0300

    HTML cadastro de produto

--- modulo-6/src/routes.js
+++ modulo-6/src/routes.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const routes = express.Router();
+const ProductController = require('./app/controllers/ProductController');
 
 routes.get('/', (req, res) => res.render("layout.njk"));
+
+routes.get('/products/create',ProductController.create );
+
+routes.get('/ads/create', (req, res) => res.redirect("/products/create"));
 
 module.exports = routes;
