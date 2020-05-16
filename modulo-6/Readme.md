e728fb58ea96a7e4cd9d3496f889a4e38deccd95
fh.proger <phillipe.souza@rte.com.br>
2020-05-16 14:59:47 -0300

    Configurando o multer

--- modulo-6/Readme.md
+++ modulo-6/Readme.md
@@ -4,6 +4,11 @@ Curso rocketseat launchbase 1.0
 # Módulo - 4 parte 2
 
 - Modelagem de dados com https://dbdiagram.io/
+- Diversas técnicas de css como exemplo montar um preview de imagem utilizando grid e oject-fit para forçar o objeto a se enquadrar no espaço.
+
+- Boas práticas js utilizando classes e arrow functions, dataTransfers para guardar os dados de arquivos arrastados (upload)
+
+- Também foi utilizado multer como middleware de upload dos arquivos
 
 ![conexao]()
 
--- modulo-6/src/routes.js
+++ modulo-6/src/routes.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const routes = express.Router();
 const ProductController = require('./app/controllers/ProductController');
+const multer = require('./app/middlewares/multer');
 
 routes.get('/', (req, res) => res.render("layout.njk"));
 
 routes.get('/products/create',ProductController.create );
 routes.get('/products/:id/edit',ProductController.edit );
 
-routes.post('/products',ProductController.post );
-routes.put('/products',ProductController.put );
+routes.post('/products', multer.array('photos', 6),ProductController.post );
+routes.put('/products',multer.array('photos', 6), ProductController.put );
 routes.delete('/products',ProductController.delete );
 
 routes.get('/ads/create', (req, res) => res.redirect("/products/create"));
