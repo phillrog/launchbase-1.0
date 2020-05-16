23d807d2437841cbf2e16ade3d729b67f24dc27f
fh.proger <phillipe.souza@rte.com.br>
2020-05-16 20:39:10 -0300

    Trigger de products

---
+++ modulo-6/migrations/20200516232518-products-trigger-before-update.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.query(`
+    CREATE FUNCTION trigger_set_tmestamp()
+    RETURNS TRIGGER AS $$ 
+    BEGIN
+      NEW.updatedAt = Now();
+      RETURN NEW;
+    END;
+    $$ LANGUAGE plpgsql;
+    
+    CREATE TRIGGER SET_TIMESTAMP 
+    BEFORE UPDATE ON "Products"
+    FOR EACH ROW 
+    EXECUTE PROCEDURE trigger_set_tmestamp();
+    
+    `);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.query(`
+      DROP FUNCTION trigger_set_tmestamp;
+      DROP TRIGGER SET_TIMESTAMP;
+    `);
+  }
+};
