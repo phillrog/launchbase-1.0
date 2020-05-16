c892bd722fe7a5e25c5f8daae7b82bea16c340e4
fh.proger <phillipe.souza@rte.com.br>
2020-05-16 18:20:45 -0300

    Exclusão de imagens do banco de dados e no gerenciador

--- modulo-6/src/app/controllers/ProductController.js
+++ modulo-6/src/app/controllers/ProductController.js
@@ -64,11 +64,23 @@ module.exports = {
         const keys = Object.keys(req.body);
 
         for (const key of keys) {
-            if (req.body[key] == "") {
+            if (req.body[key] == "" && key != "removed_files") {
                 return res.send("Please fill all fields");
             }
         }
         
+        if ( req.body.removed_files)
+        {
+            const removedFiles = req.body.removed_files.split(",");
+            const lastIndex = removedFiles.length - 1;
+
+            removedFiles.splice(lastIndex, 1);
+
+            const removedFilesPromise = removedFiles.map(id => Files.delete(id));
+
+            await Promise.all(removedFilesPromise);
+        }
+
         req.body.price = req.body.price.replace(/\D/g, '');
 
         if (req.body.price != req.body.old_price) {
