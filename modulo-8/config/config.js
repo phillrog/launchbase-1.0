0a7020023bf391406b7ef839861d83c3bcd89486
Phillipe Roger Souza <phillipe.souza@rte.com.br>
2020-05-27 16:19:48 -0300

    Ajuste configs

--- modulo-8/config/config.js
+++ modulo-8/config/config.js
@@ -1,11 +1,11 @@
 
 module.exports = {
  
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || '1234',
+  database: process.env.DB_DATABASE || 'launchstoredb',
+  host: process.env.DB_HOST || 'localhost',
   dialect: 'postgres',
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT ||'15432'
 
 }
